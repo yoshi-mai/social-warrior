c71ae6b3f5aeb70298d6ffd27835b50f7da13e29
yoshi-mai <joshua.maier94@gmail.com>
2022-11-25 17:35:15 +0100

    Added orgs component

---
+++ src/app/components/orgs/orgs.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { OrgsComponent } from './orgs.component';
+
+describe('OrgsComponent', () => {
+  let component: OrgsComponent;
+  let fixture: ComponentFixture<OrgsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ OrgsComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(OrgsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
