71253ba0e499ea6cf704cbb9e52aefd9767750d3
Julian Timo Finkenzeller <julian.finkenzeller@outlook.de>
2022-12-01 18:58:08 +0100

    added toggle und slide styling

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.scss']
+  styleUrls: ['./settings.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class SettingsComponent {
   disabled = false;
